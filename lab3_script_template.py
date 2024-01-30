1cccbdd7895abc404f40708970dddcb5f2e09a78
nick-stuckless <156607156+nick-stuckless@users.noreply.github.com>
2024-01-30 13:39:38 -0500

    update to lab

--- lab3_script_template.py
+++ lab3_script_template.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from datetime import date
+import pandas as pd
 
 def main():
     sales_csv = get_sales_csv()
@@ -27,25 +28,39 @@ def create_orders_dir(sales_csv):
     sales_csv_path = os.path.abspath(sales_csv)
     sales_csv_dir = os.path.dirname(sales_csv_path)
     # Determine the name and path of the directory to hold the order data files
-    current_date = date.today().isoformat
+    current_date = date.today().isoformat()
+    orders_folder = f"orders_{current_date}"
+    orders_directory = os.path.join(sales_csv_dir, orders_folder)
     # Create the order directory if it does not already exist
+    if not os.path.isdir(orders_directory):
+        os.makedirs(orders_directory)
 
-    return 
+    return orders_directory
 
 # Split the sales data into individual orders and save to Excel sheets
 def process_sales_data(sales_csv, orders_dir):
     # Import the sales data from the CSV file into a DataFrame
+    sales_dataframe = pd.read_csv(sales_csv)
     # Insert a new "TOTAL PRICE" column into the DataFrame
-    insert()
+    sales_dataframe = sales_dataframe.insert(7, "TOTAL PRICE", sales_dataframe["ITEM QUANTITY"] * sales_dataframe["ITEM PRICE"])
     # Remove columns from the DataFrame that are not needed
-    drop()
+    sales_dataframe.drop(columns=["ADDRESS", "CITY", "STATE", "COUNTRY", "POSTAL CODE"] inplace=True)
+    
     # Group the rows in the DataFrame by order ID
+    order_ID = sales_dataframe.sort_values(by="ORDER ID")
     # For each order ID:
+    for orders in order_ID:
         # Remove the "ORDER ID" column
+        sales_dataframe = sales_dataframe.drop(orders)
         # Sort the items by item number
+        sales_dataframe = sales_dataframe.sort_values("ITEM NUMBER")
         # Append a "GRAND TOTAL" row
+        grand_total = sales_dataframe("TOTAL PRICE").sum()
+        sales_dataframe = sales_dataframe.append("GRAND TOTAL", {grand_total})
         # Determine the file name and full path of the Excel sheet
+
         # Export the data to an Excel sheet
+
         # TODO: Format the Excel sheet
     pass
 
